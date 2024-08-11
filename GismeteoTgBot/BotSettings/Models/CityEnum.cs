329b431d8c1ea32e57240a6f993a787d7c409216
agavrilov <agavrilov@rit-it.com>
2024-08-11 23:41:59 +0700

    Добавил токен для тг бота и начал писать логику для конфиг данных работы с ботом

---
+++ GismeteoTgBot/BotSettings/Models/CityEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GismeteoTgBot.BotSettings.Models
+{
+    public enum CityEnum
+    {
+        None = 0,
+        Omsk = 4578,
+        Tomsk = 4652
+    }
+}
